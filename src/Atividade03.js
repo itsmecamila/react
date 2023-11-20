496167b0cb12a590613e7fb139e3c4172c92e9e0
itsmecamila <camilamarq.depaula@gmail.com>
2023-11-20 11:38:16 -0300

    creating Page component

---
+++ src/Atividade03.js
@@ -0,0 +1,16 @@
+import Voltar from "./Voltar.jsx";
+import TableofContents from "./TableOfContents";
+import Gallery from "./Gallery";
+
+export default function Atividade03(){
+    return(
+        <>
+        <h1>Atividade 3</h1>
+        <TableofContents/>
+        <Gallery/>
+        <Voltar/>
+        </>
+
+    )
+
+}
---
+++ src/Atividade04.js
@@ -0,0 +1,12 @@
+import Voltar from "./Voltar.jsx";
+import Calculadora from "./Calculadora";
+
+export default function Atividade04(){
+    return(
+        <>
+            <h1> Atividade 4 </h1>
+            <Calculadora/>
+            <Voltar/>
+        </>
+    )
+}
