cf270c4bd0aa09566db3f0290656f3201d07ac37
itsmecamila <camilamarq.depaula@gmail.com>
2023-11-20 17:11:18 -0300

    adding file

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const people = [{
+    id: 0,
+    name: 'Creola Katherine Johnson',
+    profession: 'mathematician',
+    accomplishment: 'spaceflight calculations',
+    imageId: 'MKeW3A'
+},
+{
+    id: 1,
+    name: 'Creola Katherine Johnson',
+    profession: 'mathematician',
+    accomplishment: 'spaceflight calculations',
+    imageId: 'mynHUSa'
+},
+{
+    id: 2,
+    name: 'Creola Katherine Johnson',
+    profession: 'mathematician',
+    accomplishment: 'spaceflight calculations',
+    imageId: 'bE7W1ji'
+},
+{
+    id: 3,
+    name: 'Creola Katherine Johnson',
+    profession: 'mathematician',
+    accomplishment: 'spaceflight calculations',
+    imageId: 'IOjWm71'
+},
+{
+    id: 4,
+    name: 'Creola Katherine Johnson',
+    profession: 'mathematician',
+    accomplishment: 'spaceflight calculations',
+    imageId: 'lrWQx8l'
+}];
