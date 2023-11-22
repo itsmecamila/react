71ba5f4ae004433487cd9c9e0cf8a9f7a94a74e7
itsmecamila <camilamarq.depaula@gmail.com>
2023-11-22 15:33:50 -0300

    editing files

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -1,42 +1,63 @@
+import React, { useState } from 'react';
 import "../css/Calculadora.css";
 
 export default function Calculadora(){
+    let [equacao,setEquacao] = useState();
+    let [resultado,setResultado] = useState();
+    // let [memoria,setEquacao] = useState();
 
-    function LimparTela(){
-        
-        
+    function LimparTela(){   
+        setResultado(0);     
     }
+
+    // function GuardarClick(botao){
+    //     if()
+    // }
+
+    function ConverterValor(){
+        setResultado(resultado * -1);
+    }
+    
+    function Porcentagem(){
+        setResultado(resultado / 100);
+    }
+
+
     return(
-        <div class="interface">
-            <div class="linha1">
-                <button>AC</button>
-                <button>+/-</button>
-                <button>%</button>
+        <div className="interface">
+            <div className="visor">
+                <p>Equação...</p>
+                <h1>{resultado}</h1>
+            </div>
+            <div className="linha1">
+                <button onClick={LimparTela}>AC</button>
+                <button onClick={ConverterValor}>+/-</button>
+                <button onClick={Porcentagem}>%</button>
                 <button>/</button>
             </div>
             
-            <div class="linha2">
+            <div className="linha2">
                 <button>7</button>
                 <button>8</button>
                 <button>9</button>
                 <button>x</button>
             </div>
             
-            <div class="linha3">
+            <div className="linha3">
                 <button>4</button>
                 <button>5</button>
                 <button>6</button>
                 <button>-</button>
             </div>
 
-            <div class="linha4">
+            <div className="linha4">
                 <button>1</button>
                 <button>2</button>
                 <button>3</button>
                 <button>+</button>
             </div>
 
-            <div class="linha5">
+            <div className="linha5">
                 <button>0</button>
                 <button>,</button>
                 <button>=</button>
